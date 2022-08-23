7a3b788fbccff4876a40a73706cd09e9c901d83b
Bram Adl <thenamesbram@gmail.com>
2022-08-24 01:45:14 +0700

    chore: create components file

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export { Billing } from "./Billing";
+export { Business } from "./Business";
+export { Button } from "./Button";
+export { CardDeal } from "./CardDeal";
+export { Clients } from "./Clients";
+export { CTA } from "./CTA";
+export { FeedbackCard } from "./FeedbackCard";
+export { Footer } from "./Footer";
+export { GetStarted } from "./GetStarted";
+export { Hero } from "./Hero";
+export { Navbar } from "./Navbar";
+export { Stats } from "./Stats";
+export { Testimonials } from "./Testimonials";

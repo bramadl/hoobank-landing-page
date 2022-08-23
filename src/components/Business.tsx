30f42efe9e4def69aabf8de57087972026142d91
Bram Adl <thenamesbram@gmail.com>
2022-08-24 02:44:10 +0700

    feat: business section component

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,5 +1,69 @@
 import React from "react";
 
+import { Button } from "./Button";
+import { features } from "../constants";
+import { styles, layout } from "../styles";
+
+type FeatureCardProps = {
+  index: number;
+  id: string;
+  icon: string;
+  title: string;
+  content: string;
+};
+
+const FeatureCard: React.FC<FeatureCardProps> = ({
+  icon,
+  title,
+  content,
+  index,
+}) => {
+  return (
+    <div
+      className={`flex flex-row p-6 rounded-[20px] ${
+        index !== features.length - 1 ? "mb-6" : "mb-0"
+      } feature-card`}
+    >
+      <div
+        className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+      >
+        <img
+          alt={title}
+          className="w-[50%] h-[50%] object-contain"
+          src={icon}
+        />
+      </div>
+      <div className="flex-1 flex flex-col ml-6">
+        <h4 className="font-poppins font-semibold text-white text-lg leading-[23px] mb-1">
+          {title}
+        </h4>
+        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">{content}</p>
+      </div>
+    </div>
+  );
+};
+
 export const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          You do the business, <br className="sm:block hidden" /> we'll handle
+          the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundred of
+          credit cards on the market.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} index={index} {...feature} />
+        ))}
+      </div>
+    </section>
+  );
 };

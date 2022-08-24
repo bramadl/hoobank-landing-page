1f4d2a62f50d1a758cd62b6efae38c02f934fb47
Bram Adl <thenamesbram@gmail.com>
2022-08-24 09:24:40 +0700

    fix: add rounded corners to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 export const Button: React.FC<ButtonProps> = ({ styles }) => {
   return (
     <button
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-lg text-primary outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-lg text-primary outline-none ${styles} rounded-[10px]`}
       type="button"
     >
       Get Started
